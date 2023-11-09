549c6036d9f6b814b0ec843d4e133d6118d2e676
João Victor Ferreira <joao.victorf02@hotmail.com>
2023-11-09 19:01:47 -0300

    chore: Configure to run dockerfile of Accumulator and Generator

--- src/QuickFixOrders.OrderAccumulator/Program.cs
+++ src/QuickFixOrders.OrderAccumulator/Program.cs
@@ -6,38 +6,39 @@ using QuickFix;
 using QuickFixOrders.Core;
 using QuickFixOrders.Data;
 
-const string httpServerPrefix = "http://127.0.0.1:5080/";
-var combinedPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "orderAccumulator.cfg");
-var app =Host.CreateDefaultBuilder()
-    .ConfigureServices((context, services) =>
+class Program
+{
+    static void Main(string[] args)
     {
-        services.AddDependencyInjectionCore();
-        services.AddDependencyInjectionData();
-        services.AddSingleton<OrderAccumulator>();
-    }).Build();
-QuickFixOrders.Data.Setup.AddInitialData(app.Services.GetRequiredService<DataContext>());
+        const string httpServerPrefix = "http://127.0.0.1:5080/";
+        var combinedPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "orderAccumulator.cfg");
+        var app = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddDependencyInjectionCore();
+                services.AddDependencyInjectionData();
+                services.AddSingleton<OrderAccumulator>();
+            }).Build();
+        QuickFixOrders.Data.Setup.AddInitialData(app.Services.GetRequiredService<DataContext>());
 
-try
-{
-    SessionSettings settings = new SessionSettings(combinedPath);
-    IApplication executorApp = app.Services.GetRequiredService<OrderAccumulator>();
-    IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
-    ILogFactory logFactory = new FileLogFactory(settings);
-    ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(executorApp, storeFactory, settings, logFactory);
-    HttpServer srv = new HttpServer(httpServerPrefix, settings);
-
-    acceptor.Start();
-    srv.Start();
-
-    Console.WriteLine("View OrderAccumulator status: " + httpServerPrefix);
-    Console.WriteLine("press <enter> to quit");
-    Console.Read();
-
-    srv.Stop();
-    acceptor.Stop();
-}
-catch (Exception e)
-{
-    Console.WriteLine("==FATAL ERROR==");
-    Console.WriteLine(e.ToString());
+        try
+        {
+            SessionSettings settings = new SessionSettings(combinedPath);
+            IApplication executorApp = app.Services.GetRequiredService<OrderAccumulator>();
+            IMessageStoreFactory storeFactory = new FileStoreFactory(settings);
+            ILogFactory logFactory = new FileLogFactory(settings);
+            ThreadedSocketAcceptor acceptor = new ThreadedSocketAcceptor(executorApp, storeFactory, settings, logFactory);
+            HttpServer srv = new HttpServer(httpServerPrefix, settings);
+            
+            Console.WriteLine("Starting Order Accumulator...");
+            acceptor.Start();
+            srv.Start();
+            Console.WriteLine("Order Accumulator started!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("==FATAL ERROR==");
+            Console.WriteLine(e.ToString());
+        }
+    }
 }
